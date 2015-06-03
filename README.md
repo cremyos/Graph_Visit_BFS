b94451027e76c964998477634f1f27a744e956ae
cremyos <dreamer_forever@126.com>
2015-06-02 17:58:05 -0700

    graph

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Graph_Visit_BFS
-Adjecancy list storage the graph and visit it though the BFS.
+Adjacency list storage the graph and visit it though the BFS.
---
+++ graph_visit_BFS.c
@@ -0,0 +1,12 @@
+/*
+ ************************************************
+ *Name : graph_visit_BFS.c                      *
+ *Date : 2015-06-03                             *
+ *Author : sniper                               *
+ *Aim : The Adjacency list storage the graph and*
+ *      visit it using the BFS.                 *
+ ************************************************
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
